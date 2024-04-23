8d0aa67d7e78cbbe947364bc31077772f2908768
C21899 <frank.soler@microchip.com>
2024-04-23 14:59:32 -0500

    go back to default theme

--- init.el
+++ init.el
@@ -2,5 +2,4 @@
 (setq make-backup-files nil) ;stop creating backup~ files
 (setq auto-save-default nil) ;stop creating #autosave# files
 
-(load-theme 'modus-vivendi) ; Dark theme
 (tool-bar-mode -1)
