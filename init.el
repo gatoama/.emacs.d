9e400fed2f0ae94ce7007201677236343ee26f94
C21899 <frank.soler@microchip.com>
2024-04-23 12:53:25 -0500

    first commit

---
+++ init.el
@@ -0,0 +1,6 @@
+;; General Settings
+(setq make-backup-files nil) ;stop creating backup~ files
+(setq auto-save-default nil) ;stop creating #autosave# files
+
+(load-theme 'modus-vivendi) ; Dark theme
+(tool-bar-mode -1)
