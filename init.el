9226569e0921ae63d51a2a5b85dc285ad136be54
C21899 <frank.soler@microchip.com>
2024-04-25 11:34:20 -0500

    appearance customizations

--- init.el
+++ init.el
@@ -3,3 +3,8 @@
 (setq auto-save-default nil) ;stop creating #autosave# files
 
 (tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(load-theme 'wombat)
+(set-face-attribute 'region nil :background "#136" :foreground "#cdb38b")
